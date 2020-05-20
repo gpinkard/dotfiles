3a9e25574b6d27fc91c8cc01bac4d3468026cb1d
gpinkard <gbrlpnkrd@gmail.com>
2020-05-19 23:05:48 -0700

    removed old bloat

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### GABRIEL'S DOT/CONFIGURATION FILES
 
-I used to use i3wm, then bspwm. Now I live in emacs. [See my emacs config](.emacs.d/config.org).
+I used to use i3wm, then bspwm; and exclusively use vi/vim/neovim. Now I live in emacs. [See my emacs config](.emacs.d/config.org).
 
 ### Screenshots (old wm workflow)
 ![Screenshot](/i3/wallpapers/workflow1.png)
--- scripts/xsetroot.sh
+++
@@ -1,6 +0,0 @@
-DATETIME=$(date +"%F %R")
-# WIFI=$()
-VOLUME=$( amixer sget Master | grep -e 'Front Left:' | \
-    sed 's/[^\[]*\[\([0-9]\{1,3\}%\).*\(on\|off\).*/\2 \1/' | sed 's/off/M/' | sed 's/on //' )
-BATTERYSTATE=$( acpi -b | awk '{ split($5,a,":"); print substr($3,0,2), $4, "["a[1]":"a[2]"]" }' | tr -d ',' )
-xsetroot -name " ${VOLUME} |  ${BATTERYSTATE} |  ${DATETIME}"
