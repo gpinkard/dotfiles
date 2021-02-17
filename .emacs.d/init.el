382f372d318f0b7268df0a71da4a95d0e2e5a70e
gpinkard <gabriel.pinkard@jupiterintel.com>
2021-02-17 11:20:26 -0800

    small tweaks for emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,9 +25,15 @@
     ("5d59bd44c5a875566348fa44ee01c98c1d72369dc531c1c5458b0864841f887c" default)))
  '(package-selected-packages
    (quote
-    (dired-rainbow dockerfile-mode elfeedb vterm kubernetes rust-mode doom-modeline exwm csv-mode evil-mode crystal-mode symon dired-collapse dired-subtree markdown-mode counsel-projectile projectile exec-path-from-shell popup-kill-ring kill-ring toml-mode lua-mode sudo-edit graphql-mode json-mode dashboard dmenu linenum-relative linum-relative docker magit rainbow-mode avy go-mode beacon ellocate company-mode)))
+    (highlight-symbol cargo dired-rainbow dockerfile-mode elfeedb vterm kubernetes rust-mode doom-modeline exwm csv-mode evil-mode crystal-mode symon dired-collapse dired-subtree markdown-mode counsel-projectile projectile exec-path-from-shell popup-kill-ring kill-ring toml-mode lua-mode sudo-edit graphql-mode json-mode dashboard dmenu linenum-relative linum-relative docker magit rainbow-mode avy go-mode beacon ellocate company-mode)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
      (lambda nil
        (ansi-term shell-pop-term-shell))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
