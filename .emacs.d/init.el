2b09792092545d4c50b1faa6aa960ce89200d04a
gpinkard <gabriel.pinkard@jupiterintel.com>
2021-06-03 10:17:51 -0700

    update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,9 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(weyland-yutani))
  '(package-selected-packages
-   '(k8s-mode ivy-avy bart-mode info-colors telephone-line org-bullets all-the-icons-dired all-the-icons-ivy all-the-icons scala-mode highlight-symbol cargo dired-rainbow dockerfile-mode elfeed vterm kubernetes rust-mode doom-modeline exwm csv-mode evil-mode crystal-mode symon dired-collapse dired-subtree markdown-mode counsel-projectile projectile exec-path-from-shell popup-kill-ring kill-ring toml-mode lua-mode sudo-edit graphql-mode json-mode dashboard dmenu linenum-relative linum-relative docker magit rainbow-mode avy go-mode beacon ellocate company-mode))
+   '(nord-theme osx-browse mu4e lsp-ivy lsp-ui lsp-mode k8s-mode ivy-avy bart-mode info-colors telephone-line org-bullets all-the-icons-dired all-the-icons-ivy all-the-icons scala-mode highlight-symbol cargo dired-rainbow dockerfile-mode elfeed vterm kubernetes rust-mode doom-modeline exwm csv-mode evil-mode crystal-mode symon dired-collapse dired-subtree markdown-mode counsel-projectile projectile exec-path-from-shell popup-kill-ring kill-ring toml-mode lua-mode sudo-edit graphql-mode json-mode dashboard dmenu linenum-relative linum-relative docker magit rainbow-mode avy go-mode beacon ellocate company-mode))
  '(shell-pop-shell-type
    '("ansi-term" "*ansi-term*"
      (lambda nil
