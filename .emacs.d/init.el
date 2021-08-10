5cc2ada3e09b54a18a1146a5ea599248028b4e46
gpinkard <gabriel.pinkard@jupiterintel.com>
2021-08-10 11:00:03 -0700

    "acme"

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(naysayer-theme osx-browse mu4e lsp-ivy lsp-ui lsp-mode k8s-mode ivy-avy bart-mode info-colors telephone-line org-bullets all-the-icons-dired all-the-icons-ivy all-the-icons scala-mode highlight-symbol cargo dired-rainbow dockerfile-mode elfeed vterm kubernetes rust-mode doom-modeline exwm csv-mode evil-mode crystal-mode symon dired-collapse dired-subtree markdown-mode counsel-projectile projectile exec-path-from-shell popup-kill-ring kill-ring toml-mode lua-mode sudo-edit graphql-mode json-mode dashboard dmenu linenum-relative linum-relative docker magit rainbow-mode avy go-mode beacon ellocate company-mode))
+   '(javascript-mode flycheck-rust rego-mode naysayer-theme osx-browse lsp-ivy lsp-ui lsp-mode k8s-mode ivy-avy bart-mode info-colors telephone-line org-bullets all-the-icons-dired all-the-icons-ivy all-the-icons scala-mode highlight-symbol cargo dired-rainbow dockerfile-mode elfeed vterm kubernetes rust-mode doom-modeline exwm csv-mode evil-mode crystal-mode symon dired-collapse dired-subtree markdown-mode counsel-projectile projectile exec-path-from-shell popup-kill-ring kill-ring toml-mode lua-mode sudo-edit graphql-mode json-mode dashboard dmenu linenum-relative linum-relative docker magit rainbow-mode avy go-mode ellocate company-mode))
  '(shell-pop-shell-type
    '("ansi-term" "*ansi-term*"
      (lambda nil
---
+++ acme/colors.h
@@ -0,0 +1,14 @@
+// column colors
+
+#define COL_BG 0x0b3335FF
+#define COL_HBG 0xC0C5CEFF
+#define COL_BORD 0xF99157FF
+#define COL_TEXT 0xD1B897FF
+#define COL_HTEXT 0x1B2B34FF
+
+// text colors
+#define TXT_BG 0x1B2B34FF
+#define TXT_HBG 0xC0C5CEFF
+#define TXT_BORD 0x4F5B66FF
+#define TXT_TEXT 0xD1B897FF
+#define TXT_HTEXT 0x1B2B34FF
