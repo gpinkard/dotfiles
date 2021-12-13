84ede51e7abd726dfa7e69c93b3770487582ed77
gpinkard <gabriel.pinkard@jupiterintel.com>
2021-12-13 14:22:28 -0800

    update

--- acme/colors.h
+++ acme/colors.h
@@ -1,15 +1,15 @@
 // https://github.com/gpinkard/dotfiles/tree/master/acme
 
 // column colors
-#define COL_BG 0x555555FF
-#define COL_HBG 0xFFFFFFFF
+#define COL_BG 0x575757FF
+#define COL_HBG 0x000000FF
 #define COL_BORD 0x0288D1FF
-#define COL_TEXT 0xE1D9C2FF
-#define COL_HTEXT 0x000000FF
+#define COL_TEXT 0xFFFFFFFF
+#define COL_HTEXT 0xFFFFFFFF
 
 // text colors
-#define TXT_BG 0xE1D9C2FF
+#define TXT_BG 0x1B2B34FF
 #define TXT_HBG 0xFFF8E1FF
 #define TXT_BORD 0x4F5B66FF
-#define TXT_TEXT 0x000000FF
-#define TXT_HTEXT 0x000000FF
+#define TXT_TEXT 0xDBDBDBFF
+#define TXT_HTEXT 0x1B2B34FF
