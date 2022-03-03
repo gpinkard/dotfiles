ac1f4eaacb0bfd2c0ef4213444a1cb373081306b
gpinkard <gbrlpnkrd@gmail.com>
2022-03-02 21:48:25 -0800

    update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(tao-theme dashboard projectile rainbow-mode shell-pop sudo-edit which-key exec-path-from-shell use-package))
  '(shell-pop-shell-type
    '("ansi-term" "*ansi-term*"
      (lambda nil
