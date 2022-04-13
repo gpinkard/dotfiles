ee54e6072a9f1ecdc0bc36aab008d3b37cd7be74
gpinkard <gabriel.pinkard@jupiterintel.com>
2022-04-13 16:27:35 -0700

    acme colors

--- acme/colors.h
+++ acme/colors.h
@@ -2,14 +2,14 @@
 
 // column colors
 #define COL_BG 0x575757FF
-#define COL_HBG 0x000000FF
+#define COL_HBG 0xFDA50FFF
 #define COL_BORD 0x0288D1FF
 #define COL_TEXT 0xFFFFFFFF
-#define COL_HTEXT 0xFFFFFFFF
+#define COL_HTEXT 0x000000FF
 
 // text colors
-#define TXT_BG 0x1B2B34FF
-#define TXT_HBG 0xFFF8E1FF
+#define TXT_BG 0xFFFFFFFF
+#define TXT_HBG 0xFDA50FFF
 #define TXT_BORD 0x4F5B66FF
-#define TXT_TEXT 0xDBDBDBFF
-#define TXT_HTEXT 0x1B2B34FF
+#define TXT_TEXT 0x000000FF
+#define TXT_HTEXT 0x000000FF
