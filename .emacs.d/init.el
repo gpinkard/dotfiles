4f6fcae4e48d08dead0d9cb0036e2ee6441c4250
gpinkard <gabriel.pinkard@jupiterintel.com>
2022-05-09 08:56:24 -0700

    emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,4 +9,4 @@
  ;; If there is more than one, they won't work right.
  '(mouse-wheel-progressive-speed nil)
  '(package-selected-packages
-   '(dmenu rustic flycheck-rust rust-mode flycheck python-mode company lsp-ui lsp-ivy lsp-mode clojure-mode counsel swiper ivy tao-theme dashboard projectile rainbow-mode sudo-edit which-key exec-path-from-shell use-package)))
+   '(kaolin-themes symon symon-mode ace-window org-superstar all-the-icons-ivy-rich ivy-rich all-the-icons-dired all-the-icons rustic flycheck-rust rust-mode flycheck python-mode company lsp-ui lsp-ivy lsp-mode clojure-mode counsel swiper ivy tao-theme dashboard projectile rainbow-mode sudo-edit which-key exec-path-from-shell use-package)))
