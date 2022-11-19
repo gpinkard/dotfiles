383c956206dce4d2a5418cfa2726860c7efc4ae5
gpinkard <gabriel.pinkard@jupiterintel.com>
2022-11-19 12:46:11 -0800

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,8 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/.emacs.d/config.org"))
  '(package-selected-packages
-   '(lsp-pyright kaolin-themes magit minions moody ivy-rich yaml-mode which-key use-package sudo-edit rainbow-mode python-mode projectile org-superstar lsp-ui lsp-ivy go-mode flycheck exec-path-from-shell elfeed dockerfile-mode dashboard counsel company clojure-mode all-the-icons-ivy-rich all-the-icons-dired ace-window)))
+   '(multi-vterm vterm bart-mode cider srcery-theme lsp-pyright kaolin-themes magit minions moody ivy-rich yaml-mode which-key use-package sudo-edit rainbow-mode python-mode projectile org-superstar lsp-ui lsp-ivy go-mode flycheck exec-path-from-shell elfeed dockerfile-mode dashboard counsel company clojure-mode all-the-icons-ivy-rich all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
